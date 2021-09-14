8fb0d21e6bd11b6f33638d8fdaa3a67bf25c74ba
figure <peizhenfei@cvte.com>
2021-09-14 21:26:22 +0800

    定制测试镜像

---
+++ docker-builder/app.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+DB_NAME = os.environ.get('DB_NAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+
+
+@app.route("/")
+def hello_world():
+    return f"""
+    <p>Hello, World!</p>
+    <p>---something important from environ---</p>
+    <p>DB_NAME={DB_NAME}</p>
+    <p>DB_PASSWORD={DB_PASSWORD}</p>
+    <p>DB_HOST={DB_HOST}</p>
+    <p>DB_PORT={DB_PORT}</p>
+    """
